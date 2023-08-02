d4ce69689f203ac838193569b12622819820250e
rambip <apero1808@gmail.com>
2023-08-02 18:31:13 +0200

    add error and changed error html formating

---
+++ examples/onclick/src/main.rs
@@ -0,0 +1,54 @@
+use leptos::*;
+use leptos_markdown::{Markdown, MarkdownMouseEvent};
+
+static MARKDOWN_SOURCE : &str = r#"
+# Interactive markdown experiment
+## Goal
+This page illustrates how you can use the `onclick` property of the `Markdown` component in order to add some interactivity in your markdown
+
+## Usage
+Test for yourself: click on any text on this page and it will appear highlighted in the source
+
+
+# Click me
+
+| title 1 | title 2 |
+| ------- | ------- |
+| **bold**|  `code` |
+
+$$\sum_{n=-\infty}^{+\infty}c_n e^{inx}$$
+
+> Quote
+
+"#;
+
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (position, set_position) = create_signal(cx, 0..0);
+
+    let onclick = move |e: MarkdownMouseEvent| set_position(e.position);
+
+    let before = move || &MARKDOWN_SOURCE[0..position().start];
+    let middle = move || &MARKDOWN_SOURCE[position()];
+    let after =  move || &MARKDOWN_SOURCE[position().end..];
+
+    view!{cx,
+        <div>
+            <Markdown src=MARKDOWN_SOURCE on_click=Box::new(onclick)/>
+                <br/>
+                <hr/>
+                <p>{"markdown source:"}</p>
+                <pre style={"border: 2px solid orange"}>
+                {before}
+                <span style={"background-color: orange"}>{middle}</span>
+                {after}
+                </pre>
+        </div>
+    }
+}
+
+fn main() {
+    console_error_panic_hook::set_once();
+    mount_to_body(|cx| view!{cx, <App/>})
+}
